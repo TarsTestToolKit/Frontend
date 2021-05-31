b836e6e7d4ef0959bfc165c7478e2d0f5863d56c
zhaoguolei1024 <admin@zhaoguolei.com>
2021-05-31 16:27:26 +0800

    update

--- client/src/components/HistoryContent.vue
+++ client/src/components/HistoryContent.vue
@@ -299,7 +299,7 @@
       </el-tab-pane>
       
     </el-tabs>
-    <el-backtop target="#pane-0 .tabcontent .el-scrollbar__wrap"></el-backtop>
+    <el-backtop target=".el-scrollbar__wrap"></el-backtop>
   </el-drawer>
 </template>
 <script lang="ts">
