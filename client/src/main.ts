94494c5412572a5989399fd0eba73fa395786064
zhaoguolei1024 <admin@zhaoguolei.com>
2021-06-01 15:30:01 +0800

    update

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,10 +4,11 @@ import router from './router'
 //完整引入elementplus组件
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css';
+import locale from 'element-plus/lib/locale/lang/zh-cn'
 
 const app=createApp(App)
 
-app.use(ElementPlus)
+app.use(ElementPlus,{locale})
 app.use(router)
 app.mount('#app')
 
