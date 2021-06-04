763f9725d750f02f6a7df5f2e1c3fde75db60238
ruanshudong <ruanshudong@qq.com>
2021-06-04 15:32:35 +0800

    update dist

---
+++ dist/config/webConf.ts
@@ -0,0 +1,18 @@
+
+import path from "path";
+
+const webConf = {
+    
+    config: {
+        webConf: {
+            port: 3000,
+            loggerPath: "./log",
+            logFileKeepDays: "1",
+            defaultLanguage: "cn",
+            title: "文档"
+        }
+    }
+
+};
+
+export default webConf;
