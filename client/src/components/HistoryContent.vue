ecc6127864faeaa7721b31e9f22e56fcbfea8ae0
zhaoguolei1024 <admin@zhaoguolei.com>
2021-06-24 11:02:43 +0800

    update

--- client/src/components/HistoryContent.vue
+++ client/src/components/HistoryContent.vue
@@ -18,7 +18,7 @@
     
     
   </div>
-  <el-dialog title="测试结果" v-model="FunctionaldialogTableVisible.data" width="60%">
+  <el-dialog title="测试结果" v-model="FunctionaldialogTableVisible.data" width="70%">
     <el-table v-if="DoFuncTestresult.code!=-1" :data="DoFuncTestresult.rows" border :header-cell-style="{background:'#F9FAFC'}">
       <el-table-column type="index" width="50"></el-table-column>
       <el-table-column property="startTime" :formatter="formatdate" label="开始时间"></el-table-column>
